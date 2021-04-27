a8f5123c7ba6164a7dde5f7393ce7bb9b3ae61cd
jorgevz <jorgev2132@gmail.com>
2021-04-26 22:55:06 -0400

    final working code in the main branch set !

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ let numberToWords = require('number-to-words');
 
 
 
-let arrayOne = [ numberToWords.toWords(51),  numberToWords.toWords(55) ]
-let arrayTwo = [numberToWords.toWords(88),  numberToWords.toWords(45)]
+let arrayOne = [ numberToWords.toWords(51),  numberToWords.toWords(55),numberToWords.toWords(14) ]
+
+let arrayTwo = [numberToWords.toWords(88),  numberToWords.toWords(45), numberToWords.toWords(25)]
 
 console.log(arrayOne);
 console.log(arrayTwo);
